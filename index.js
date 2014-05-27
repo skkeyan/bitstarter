8fed4de2b28a8ffa29e9332bf8f08579beb154ff
skkeyan <skkeyan@gmail.com>
2014-05-27 09:34:41 +0000

    Reading index.html v0.1

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  fs.readFileSync('index.html')
+  display_on_website = buffer.toString("utf-8",0,22)
+  response.send(display_on_website)
 })
 
 app.listen(app.get('port'), function() {
