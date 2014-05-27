1d3cb4e4623990af869a55526b4622de83c5e4db
skkeyan <skkeyan@gmail.com>
2014-05-27 09:47:24 +0000

    Bug Fixing index.js 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html')
+  buffer = fs.readFileSync('index.html')
   display_on_website = buffer.toString("utf-8",0,22)
   response.send(display_on_website)
 })
