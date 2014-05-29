d69cae33d1c8d199f0a43ba32abfd3667131f879
skkeyan <skkeyan@gmail.com>
2014-05-29 13:37:36 +0000

    Removed response.end in index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ http.createServer(function(request,response) {
     var html = fs.readFileSync(htmlfile).toString();
     response.writeHead('content-type','text/plain');
     response.write(html);
-    response.end();
 }).listen(process.env.PORT || 8080);
 
 console.log('Listening on port 8080...');
